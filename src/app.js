f134c588ed1d1b6280a4781668719d5b20ded623
Sara Fletcher <saflebri@gmail.com>
2023-03-08 17:22:28 -0500

    Fix Update server express

--- src/app.js
+++ src/app.js
@@ -17,32 +17,58 @@ const env = async () => {
         stock: 25
     }
 
-    let result = await inventary.addProduct(newProduct);
+    /* let result = await inventary.addProduct(newProduct);
     console.log(result);
     let consult = await inventary.getProducts();
     console.log(consult);
-    let consult2 = await inventary.getProductById(1);
-
-
-    let consult3 = await inventary.updateProduct(2, 'title', 'gaseosa');
-
-    let consult4 = await inventary.deleteProduct(1);
+    let consult2 = await inventary.getProductById(1); 
+    let consult3 = await inventary.updateProduct(1, 'title', 'gaseosa');
+    /* let consult4 = await inventary.deleteProduct(2); */
 
 }
 
 env();
 
-/* 
+
 const app = express();
 
-const products = inventary.existProduct();
+const envGet = async () => {
+
+    const data = await inventary.existProduct();
+    /* 
+        app.get("/products", (req, res) => {
+    
+            res.send(data);
+        });*/
 
 
-app.get("/", (req, res) => {
+    app.get("/products", (req, res) => {
 
-    res.send({ products: [] });
-});
+        const limit = req.query.limit;
 
-app.listen(3005, () => {
-    console.log("Servidor arriba en el puerto 8080")
-}); */
+        if (limit === 0) return res.send(data);
+        const result = data.slice(0, limit);
+        res.send(result);
+
+    })
+
+
+    app.get("/:pid", (req, res) => {
+
+        let pid = req.params.pid;
+        let product = data.find((item) => item.id === pid);
+
+        if (!product) {
+            res.send({ error: "Producto no encontrado" })
+        } else {
+            res.send(product);
+        }
+    })
+
+
+    app.listen(3005, () => {
+        console.log("Servidor arriba en el puerto 8080")
+    });
+}
+
+envGet(); 
